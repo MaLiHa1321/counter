7b9eabe684c6b0084beaa603aa79dd7c06cd9e48
MaLiHa1321 <malihasultana2001@gmail.com>
2025-02-23 19:24:34 +0600

    border added

--- src/UserDetails.jsx
+++ src/UserDetails.jsx
@@ -1,9 +1,9 @@
 export default function UserDetails({detail}){
     const {name, email} = detail
     return (
-        <>
+        <div style={{border:'2px solid green'}}>
         <h2>Name: {name}</h2>
         <h3>Email: {email}</h3>
-        </>
+        </div>
     )
 }
