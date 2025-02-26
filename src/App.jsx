45b728cbfaddfb17ed4c078812afdafb9dea9c18
MaLiHa1321 <malihasultana2001@gmail.com>
2025-02-26 20:49:57 +0600

    file changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,30 @@
 import { useState } from 'react'
 
 import './App.css'
-import Counter from './Counter';
-import User from './User';
+// import Counter from './Counter';
+// import User from './User';
 
 function App() {
 
-  function handleClick(){
-    console.log("clicked the button");
-    alert('button clicked')
-  }
+  // function handleClick(){
+  //   console.log("clicked the button");
+  //   alert('button clicked')
+  // }
 
-  function addToFour (num){
-    console.log(num + 5);
-  }
+  // function addToFour (num){
+  //   console.log(num + 5);
+  // }
 
 
   return (
     <>
  
-      <h1>Vite + React</h1>
+      {/* <h1>Vite + React</h1>
       <Counter></Counter>
       <User></User>
       <button onClick={handleClick}>Click me</button>
      
-      <button onClick={() => addToFour(4)}>Click me</button>
+      <button onClick={() => addToFour(4)}>Click me</button> */}
      
     </>
   )
